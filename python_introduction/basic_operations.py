cee188b0759f80208e240f1c5e855da6e6d0df78
mohamedshmela <mohamedshmela339@gmail.com>
2024-06-19 05:16:42 +0300

    ending the first file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+substraction = number1 - number2
+multiplication = number1 * number2
+print("addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", substraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
