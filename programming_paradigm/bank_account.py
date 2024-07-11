e3943d9eb5c58a7a0c90612f483ff7eb6ce6dcdc
mohamedshmela <mohamedshmela339@gmail.com>
2024-07-11 17:57:30 +0300

    finishing the first file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,3 +1,19 @@
-class bank:
-    def __init__(self) -> None:
-        pass
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self,amount):
+        self.account_balance += amount
+
+    def withdraw(self,amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False 
+    
+    def display_balance(self):
+        print(f"the account balance is {self.account_balance}")
+
+
+
