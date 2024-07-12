c311e11a434927fbe7ac557856d86c9566c359a3
mohamedshmela <mohamedshmela339@gmail.com>
2024-07-12 04:12:21 +0300

    new change

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,7 +2,10 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.is_checked_out = False 
+        self.is_checked_out = False
+
+    def return_book(self):
+        pass 
 
 class Library:
 
