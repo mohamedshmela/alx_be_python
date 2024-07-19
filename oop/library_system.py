694d1d690d399ab16a1eaa69bbbbcfbea12313e5
mohamedshmela <mohamedshmela339@gmail.com>
2024-07-19 05:24:10 +0300

    making the first change for the second file

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,15 +3,24 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self) -> str:
+        return f"Book: {self.title} by {self.author}"
+
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self) -> str:
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self) -> str:
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self, books: list = []):
@@ -22,10 +31,5 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            if type(book) is PrintBook:
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            elif type(book) is EBook:
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+            print(book)
 
